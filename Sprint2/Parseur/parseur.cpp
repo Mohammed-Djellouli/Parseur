6dbf8d76c160adea412ffa53e358aa729bc01a3f
YoussefJEMAA <youssef.jemaa@alumni.univ-avignon.fr>
2025-04-20 16:26:48 +0200

    lecture fichiers .txt et renommage noms sans espaces
    lecture fichiers .txt et renommage noms sans espaces

--- Sprint2/Parseur/parseur.cpp
+++ Sprint2/Parseur/parseur.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <filesystem>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 namespace fs = std::filesystem;
@@ -9,22 +12,40 @@ int main() {
     string dossierEntree = "converted_txt";
     string dossierSortie = "parsed_txt";
 
-    //Supprimer le dossier de sortie s’il existe déjà
+    // Supprimer le dossier de sortie sâ€™il existe dÃ©jÃ 
     if (fs::exists(dossierSortie)) {
         cout << "Suppression de l'ancien dossier '" << dossierSortie << "'..." << endl;
         fs::remove_all(dossierSortie);
     }
 
-    //Créer le dossier parsed_txt
-    cout << "Création du dossier '" << dossierSortie << "'..." << endl;
+    // CrÃ©er le dossier parsed_txt
+    cout << "CrÃ©ation du dossier '" << dossierSortie << "'..." << endl;
     fs::create_directory(dossierSortie);
 
-    //Parcourir les fichiers dans converted_txt
-    cout << "Fichiers trouvés dans '" << dossierEntree << "':" << endl;
+    // Parcourir les fichiers dans converted_txt
+    cout << "Fichiers trouvÃ©s dans '" << dossierEntree << "':" << endl;
+
     for (const auto& fichier : fs::directory_iterator(dossierEntree)) {
         if (fichier.path().extension() == ".txt") {
-            cout << "- " << fichier.path().filename() << endl;
-            //Ici tu pourras appeler d’autres fonctions sur chaque fichier
+            string nomOriginal = fichier.path().filename().string();
+            cout << "- " << nomOriginal << endl;
+
+            // Remplacer les espaces par des underscores
+            string nomModifie = nomOriginal;
+            replace(nomModifie.begin(), nomModifie.end(), ' ', '_');
+
+            //Lire le contenu du fichier
+            ifstream fichierEntree(fichier.path());
+            if (!fichierEntree) {
+                cerr << "Erreur lors de l'ouverture du fichier : " << nomOriginal << endl;
+                continue;
+            }
+
+            stringstream buffer;
+            buffer << fichierEntree.rdbuf();
+            string contenu = buffer.str();
+
+
         }
     }
 
